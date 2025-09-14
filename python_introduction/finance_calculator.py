aff044cb44b7bb99e3d35d31db4acae982c916cb
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2025-09-14 22:19:02 +0300

    Add finance calculator for monthly savings
    Calculate monthly savings and projected savings based on income and expenses.

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,5 @@
+monthly_income = int(input("Enter your monthly income:"))
+monthly_expenses = int(input("Enter your monthly expenses:"))
+monthly_saving = monthly income - monthly_expenses
+print("Your monthly saving are ", monthly_saving)
+projected_saving = monthly_saving * 12 + (monthly_saving * 12 * 0.05)
