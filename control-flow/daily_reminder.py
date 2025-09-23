83c498379d6ca7ff2c42a439c8cd7a8482bc7c8f
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2025-09-23 17:26:46 +0300

    Refactor match cases for priority and time-bound

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -2,11 +2,11 @@ task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ").lower()
 time_bound = input("Is it time-bound? (yes/no): ").lower()
 match priority:
-  case x if x == "high" and time_bound == "yes":
+  case "high" and time_bound == "yes":
             print("Reminder: ","'",tasl,"'", " is a high priority task that requires immediate attention today!")
-  case x if x == "low" and time_bound == "yes":
+  case "low" and time_bound == "yes":
              print("Reminder: ","'",tasl,"'", " is a low priority task that requires immediate attention today!")
-  case x if x == "high" and time_bound == "no":
+  case "high" and time_bound == "no":
             print("Reminder: ","'",tasl,"'", " is a low priority task. Consider completing it when you have free time.")
-  case x if x == "low" and time_bound == "no":
+  case "low" and time_bound == "no":
              print("Reminder: ","'",tasl,"'", " is a low priority task. Consider completing it when you have free time.")
