4fe7c1c30be0b32a7f45b58596d8e3249308f6c0
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2025-09-23 17:25:25 +0300

    Refactor daily reminder logic for priority checks

--- control-flow/daily_reminder.py
+++ control-flow/daily_reminder.py
@@ -1,12 +1,12 @@
 task = input("Enter your task: ")
 priority = input("Priority (high/medium/low): ").lower()
 time_bound = input("Is it time-bound? (yes/no): ").lower()
-match time_bound:
-  case x if x == "yes" and priority == "high":
+match priority:
+  case x if x == "high" and time_bound == "yes":
             print("Reminder: ","'",tasl,"'", " is a high priority task that requires immediate attention today!")
-  case x if x == "yes" and priority == "low":
+  case x if x == "low" and time_bound == "yes":
              print("Reminder: ","'",tasl,"'", " is a low priority task that requires immediate attention today!")
-  case x if x == "no" and priority == "high":
+  case x if x == "high" and time_bound == "no":
             print("Reminder: ","'",tasl,"'", " is a low priority task. Consider completing it when you have free time.")
-  case x if x == "no" and priority == "low":
+  case x if x == "low" and time_bound == "no":
              print("Reminder: ","'",tasl,"'", " is a low priority task. Consider completing it when you have free time.")
