018ee8a82203b15f90a2426f08a44778bba741c0
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2025-09-28 14:03:02 +0300

    Create arithmetic_operations.py

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,15 @@
+def perform_operation(num1,num2,operation):
+  if operation == "add":
+    return num1 + num2
+  elif opration == "subtract":
+    return num1 - num2
+  elif opration == "multiply":
+    return num1 * num2
+  elif opration == "divide":
+    if num2 != 0 :
+      return num1 / num2
+    else:
+      return "error"
+    
+  
+  
