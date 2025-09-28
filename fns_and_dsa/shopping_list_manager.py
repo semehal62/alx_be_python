635fb8ecd6e43a4d6bef78ba756881ec51e30af5
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2025-09-28 14:28:36 +0300

    Create shopping_list_manager.py

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,30 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            item_add = input("enter the item : ")
+            shoppint_list.append(item_add)
+        
+        elif choice == '2':
+            item_remove = input("enter the item : ")
+            shopping_list.remove( item_removed)       
+        elif choice == '3':
+            print(shoping_list)
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
