5fe9a960ba8050f9972975acda2ed8df08f8f484
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2025-09-28 14:55:56 +0300

    Fix future date calculation and improve formatting

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,10 +1,10 @@
-from datetime import datetime, date, time, timedelta
-def display_current_datetime ():
-  current_date = datetime.now()
-  print(f"Current date and time: { current_date} ")
-def calculate_future_date ():
-  number = input("Enter the number of days to add to the current date: ")
-  future_date = date.today()
-   future_date += number
+from datetime import datetime, date, timedelta
 
-  print(f"Future date: {future_date}")
+def display_current_datetime():
+    current_date = datetime.now()
+    print(f"Current date and time: {current_date}")
+
+def calculate_future_date():
+    number = int(input("Enter the number of days to add to the current date: "))
+    future_date = date.today() + timedelta(days=number)
+    print(f"Future date: {future_date}")
