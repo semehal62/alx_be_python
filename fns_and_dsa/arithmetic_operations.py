bfb376d6638cd86e400433e333f64154b6d6ae7b
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2025-09-28 14:10:44 +0300

    Fix perform_operation function definition and typo
    Fix function definition syntax and correct typo in 'operation'.

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,4 +1,4 @@
-def perform_operation [ num1 , num2 , operation ]:
+def perform_operation ( num1 , num2 , operation ):
   if operation == "add":
     return num1 + num2
   elif opration == "subtract":
@@ -11,5 +11,4 @@ def perform_operation [ num1 , num2 , operation ]:
     else:
       return "error"
     
-  
   
