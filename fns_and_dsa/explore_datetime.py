77397cb084ced26fd649538d4c3434300af7411d
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2025-09-28 14:58:32 +0300

    Fix current date retrieval in display function

--- fns_and_dsa/explore_datetime.py
+++ fns_and_dsa/explore_datetime.py
@@ -1,8 +1,10 @@
 from datetime import datetime, date, timedelta
 
+
 def display_current_datetime():
-    current_date = dt.strftime("%Y-%m-%d %H:%M:%S")
+    current_date = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
     print(f"Current date and time: {current_date}")
+
 
 def calculate_future_date():
     number = int(input("Enter the number of days to add to the current date: "))
