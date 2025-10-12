8885f77bd425c3a12d77f5687c2b9e3fd0a1b4fb
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2025-10-12 16:00:39 +0300

    Add withdraw and display_balance methods
    Implemented withdraw and display_balance methods.

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,7 +1,7 @@
 class  BankAccount:
     def __init__(self ,account_balance = 0 ):
         self.account_balance = account_balance
-    Implement , withdraw(amount), and display_balance() methods
+    
     def deposit(self,amount):
         self.amount = amount
         self.account_balance += amount
