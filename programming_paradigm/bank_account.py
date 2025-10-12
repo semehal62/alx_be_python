800bc5df6d838b7bbae65828338a9e568c81da38
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2025-10-12 16:05:05 +0300

    Fix balance display formatting in bank_account.py

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -14,6 +14,6 @@ class  BankAccount:
         else:
             return False
     def display_balance(self):
-        self.account_balance = float( self.account_balance)
-        print(f"Current Balance: ${self.account_balance}")
+        acc = f "self.account_balance :.2f"
+        print(f"Current Balance: ${acc}")
        
