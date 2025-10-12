238a1cfa1b3845728ea9bdb00cd5f31964791338
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2025-10-12 15:08:51 +0300

    Update library_management.py

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,5 +1,5 @@
 class Book:
-    def __init__ (self, title,author, _is_checked_out):
+    def __init__ (self, title, author, _is_checked_out):
         self.title =  title
         self.title = author
         self.__is_checked_out =  _is_checked_out
