9a5572042af5f85a723604e2a7dab4439e0ad644
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2025-10-12 15:04:53 +0300

    Add Book and Library classes with methods
    Refactor library management system by adding Book and Library classes with initial methods.

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -1,24 +1,21 @@
-from library_management import Book, Library
+class Book():
+    def __init__ (self, title,author, _is_checked_out):
+        self.title =  title
+        self.title = author
+        self.__is_checked_out =  _is_checked_out
 
-def main():
-    # Setup a small library
-    library = Library()
-    library.add_book(Book("Brave New World", "Aldous Huxley"))
-    library.add_book(Book("1984", "George Orwell"))
 
-    # Initial list of available books
-    print("Available books after setup:")
-    library.list_available_books()
 
-    # Simulate checking out a book
-    library.check_out_book("1984")
-    print("\nAvailable books after checking out '1984':")
-    library.list_available_books()
-
-    # Simulate returning a book
-    library.return_book("1984")
-    print("\nAvailable books after returning '1984':")
-    library.list_available_books()
-
-if __name__ == "__main__":
-    main()
+class Library():
+    def __init__(self,_books):
+        self.books = books
+    
+    def add_book():
+        pass
+    def check_out_book(title):
+        pass
+    def  return_book(title):
+        pass
+    def list_available_books():
+        pass
+    
