f68e518132a339cdfce7dcaedd9060f9ba6a4d1e
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2025-10-12 15:47:18 +0300

    Remove print statements from bank_account methods
    Removed print statements from deposit and withdraw methods.

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -5,14 +5,12 @@ class  BankAccount:
     def deposit(self,amount):
         self.amount = amount
         self.account_balance += amount
-        print(f"Deposited: ${amount}")
+       
     def withdraw(self,amount):
         self.amount = amount
         if ( self.account_balance >= amount):
-            print(f"Withdrew: ${amount}")
             return True
         else:
-            print("Insufficient funds.")
             return False
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance}")
