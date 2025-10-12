26fbe89ed0b0510c2e230c538c89c7b220389bbc
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2025-10-12 23:07:38 +0300

    Refactor Book class and its subclasses
    Fix string representation methods and constructor calls for Book, EBook, and PrintBook classes.

--- oop/library_system.py
+++ oop/library_system.py
@@ -3,19 +3,25 @@ class Book:
     self.title = title
     self.author = author
 
+  def __str__(self):
+    return return f"{self.title} by {self.author}"
 
 class EBook(Book):
   def __init__(self, file_size, title, author):
-    super().__init__(title)
-    super().__init(author)
+    super().__init__(title,author)
     self.file_size = file_size
-
+    
+  def __str__(self):
+    return f"{self.title} by {self.author}, file size: {self.file_size}"
+    
 class PrintBook(Book):
   def __init__(self, page_count, title, author):
-    super().__init__(title)
-    super().__init(author)
+    super().__init__(title,author)
     self.page_count = page_count
 
+  def __str__(self):
+    return f"{self.title} by {self.author}, page count: {self.page_count}"
+    
 class Lirary:
   def __init__(self):
     self.books = Book()
