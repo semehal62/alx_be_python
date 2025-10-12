caa844e9a3bfc786f57b7bc7cf9d4c50ccab6486
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2025-10-12 15:44:03 +0300

    Add methods for deposit, withdraw, and balance display
    Implemented deposit, withdraw, and display_balance methods for bank account management.

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -5,11 +5,14 @@ class  BankAccount:
     def deposit(self,amount):
         self.amount = amount
         self.account_balance += amount
+        print(f"Deposited: ${amount}")
     def withdraw(self,amount):
         self.amount = amount
         if ( self.account_balance >= amount):
+            print(f"Withdrew: ${amount}")
             return True
         else:
+            print("Insufficient funds.")
             return False
     def display_balance(self):
         print(f"Current Balance: ${self.account_balance}")
