97d960349b4d393d3af4bf4106af55ed9d7b14dd
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2025-10-12 14:36:33 +0300

    Fix error messages in robust division calculator

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -5,11 +5,11 @@ def safe_divide(numerator, denominator):
         result = numerator / denominator
 
     except ZeroDivisionError as e:
-        print(f"Error: cannot divied by zero.")
+        print("Error: Cannot divide by zero.")
     except ValueError as e:
         print(f"Error: {e}")
     except TypeError :
-        print(f"Error: Please enter numeric values only.")
+        print("Error: Please enter numeric values only.")
     else:
         print(f"The result of the division is {result}")
     
