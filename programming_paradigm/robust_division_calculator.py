a9f512ea51ff3de62bd6cf8b61fd5de123d38154
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2025-10-12 14:39:42 +0300

    Modify error handling to return messages instead of print

--- programming_paradigm/robust_division_calculator.py
+++ programming_paradigm/robust_division_calculator.py
@@ -5,13 +5,13 @@ def safe_divide(numerator, denominator):
         result = numerator / denominator
 
     except ZeroDivisionError as e:
-        print("Error: Cannot divide by zero.")
+        return "Error: Cannot divide by zero."
     except ValueError as e:
-        print(f"Error: {e}")
+        return f"Error: {e}"
     except TypeError :
-        print("Error: Please enter numeric values only.")
+        return "Error: Please enter numeric values only."
     else:
-        print(f"The result of the division is {result}")
+        return f"The result of the division is {result}"
     
 
 
