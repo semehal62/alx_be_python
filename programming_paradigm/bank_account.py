c911d866acea82753a50b60bf9478791d11e1e4a
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2025-10-12 15:54:40 +0300

    Update BankAccount constructor to accept initial balance
    Modified the constructor to accept an initial account balance.

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -1,6 +1,6 @@
 class  BankAccount:
-    def __init__(self ):
-        self.account_balance = 0
+    def __init__(self ,account_balance = 0 ):
+        self.account_balance = account_balance
     Implement , withdraw(amount), and display_balance() methods
     def deposit(self,amount):
         self.amount = amount
