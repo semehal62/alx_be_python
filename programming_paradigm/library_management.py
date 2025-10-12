309c5ef693065d1f6bc2dfbe11ce58b0b367c1ee
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2025-10-12 15:27:03 +0300

    Update library_management.py

--- programming_paradigm/library_management.py
+++ programming_paradigm/library_management.py
@@ -4,18 +4,19 @@ class Book:
         self.author = author
         self.__is_checked_out =  _is_checked_out
 
+    def check_out(self):
+        pass
 
-
-class Library:
+class Library(Book):
     def __init__(self):
         self.books = books
     
-    def add_book(self):
+    def add_book(self, self.title, self.author):
         pass
     def check_out_book(self,title):
         pass
-    def  return_book(self):
+    def  return_book(self,):
         pass
-    def list_available_booksself(self):
-        pass
+    def list_available_booksself(self, self.title, self.author):
+        print(f"{self.title} by {self.author})
     
