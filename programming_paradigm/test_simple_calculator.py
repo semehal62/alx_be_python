45e451a381ecf34b46e22f424ad5e315d0eef13d
Semehal Hailu Woldesilassie <semehalhailu4@gmail.com>
2025-10-13 14:56:18 +0300

    Update test_simple_calculator.py

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,22 +1,26 @@
-# simple_calculator.py
+import unittest
+from simple_calculator import SimpleCalculator
 
-class SimpleCalculator:
-    """A simple calculator class that supports basic arithmetic operations."""
+class test_Simplecalculator(unitest.TestCase)
 
-    def add(self, a, b):
-        """Return the addition of a and b."""
-        return a + b
+    def test_add(self):
+        self.assertEqual( SimpleCalculator.add(10,5),15)
+        self.assertEqual( SimpleCalculator.add(-1,5),4)
+        self.assertEqual( SimpleCalculator.add(0,0),0)
+    def test_subtract(self):
+        self.assertEqual( SimpleCalculator.add(10,5),5)
+        self.assertEqual( SimpleCalculator.add(1,1),0)
+        self.assertEqual( SimpleCalculator.add(1,-1),2)
 
-    def subtract(self, a, b):
-        """Return the subtraction of b from a."""
-        return a - b
+    def test_multiply(self):
+        self.assertEqual( SimpleCalculator.add(10,5),150)
+        self.assertEqual( SimpleCalculator.add(10,0),0)
+        self.assertEqual( SimpleCalculator.add(0,0),0)
 
-    def multiply(self, a, b):
-        """Return the multiplication of a and b."""
-        return a * b
+    def test_divide(self):
+        self.assertEqual( SimpleCalculator.add(10,5),2)
+        self.assertEqual( SimpleCalculator.add(10,10),1)
+        self.assertEqual( SimpleCalculator.add(10,5),"None")
 
-    def divide(self, a, b):
-        """Return the division of a by b. Returns None if b is zero."""
-        if b == 0:
-            return None
-        return a / b
+if _ _name_ _ == '_ _main_ _':
+    unittest.main()
